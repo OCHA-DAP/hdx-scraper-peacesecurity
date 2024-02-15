8cb08e621a274ae197a24b401617ff4de755bdc5
Briar <briar.j.mills@gmail.com>
2024-02-15 11:28:39 -0500

    Remove hdx_site

--- run.py
+++ run.py
@@ -85,7 +85,6 @@ def main(save: bool = False, use_saved: bool = False) -> None:
 if __name__ == "__main__":
     facade(
         main,
-        hdx_site="stage",
         user_agent_config_yaml=join(expanduser("~"), ".useragents.yaml"),
         user_agent_lookup=lookup,
         project_config_yaml=join("config", "project_configuration.yml")
